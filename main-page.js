e80dffcae618b55c260eb74d87636482f1993577
Artem Kopytok <13ar.bass@gmail.com>
2022-02-09 11:52:52 +0300

    updated: telepor text

---
+++ main-page.js
@@ -0,0 +1,4 @@
+let teleportOut = document.querySelector('[cm-teleport-text="out-1"]');
+let teleportIn = document.querySelector('[cm-teleport-text="in-1"]');
+
+teleportIn.textContent = teleportOut.textContent;
