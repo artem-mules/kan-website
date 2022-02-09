49b060dc478c4183985db3db9076bb09cc1f4246
Artem Kopytok <13ar.bass@gmail.com>
2022-02-10 01:00:13 +0300

    updated: tab content script

--- main-page.js
+++ main-page.js
@@ -9,18 +9,31 @@ let timerWaiter;
 
 function startTabContent() {
     let allTabLinks = document.querySelectorAll('.tabs-menu__link');
+    let allTabPanes = document.querySelectorAll('.tabs-menu__pane');
+    let allContentRows = document.querySelectorAll('.cl-i__tabs-content');
 
-    allTabLinks.forEach(tabLink => {
+    allTabLinks.forEach((tabLink, tabId) => {
+        let appendWaiterPane = allTabPanes[tabId];
         let currentCategory = tabLink.querySelector('[data-tab-name]').getAttribute('data-tab-name');
-        console.log(currentCategory);
+        allContentRows.forEach(contentRow => {
+            let rowCategory = contentRow.querySelector('[data-row-category]').getAttribute('data-row-category');
+            if (rowCategory == currentCategory) {
+                let clonableRow = contentRow;
+                appendWaiterPane.append(clonableRow);
+            }
+        });
     });
+
+    setTimeout(() => {
+        clearTimeout(timerWaiter);
+    }, 300);
 }
 
 function tryToStartScript() {
     clearTimeout(timerWaiter);
     timerWaiter = setTimeout(() => {
         startTabContent();
-    }, 1000);
+    }, 500);
 }
 
 const targetNode = document.querySelector("#tab-section");
